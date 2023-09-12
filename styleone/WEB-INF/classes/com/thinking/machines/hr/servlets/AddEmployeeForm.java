ebf354a54ff129590ab201a1951ff578c1b7f751
Aakash-Tamboli <athanhuntak@gmail.com>
2023-09-12 08:36:41 +0530

    Assignement Complete

---
+++ styleone/WEB-INF/classes/com/thinking/machines/hr/servlets/AddEmployeeForm.java
@@ -0,0 +1,247 @@
+package com.thinking.machines.hr.servlets;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+import java.io.*;
+import com.thinking.machines.hr.dl.*;
+
+public class AddEmployeeForm extends HttpServlet
+{
+public void doGet(HttpServletRequest request,HttpServletResponse response)
+{
+try
+{
+PrintWriter pw;
+pw=response.getWriter();
+response.setContentType("text/html");
+
+pw.println("<!DOCTYPE HTML>");
+pw.println("<html lang='en'>");
+pw.println("<head>");
+pw.println("<meta charset='utf-8'>");
+pw.println("<title>HR Application</title>");
+pw.println("<script>");
+pw.println("function validateForm(frm)");
+pw.println("{");
+pw.println("var firstInvalidComponent=null;");
+pw.println("var valid=true;");
+pw.println("var name=frm.name.value.trim();");
+pw.println("var nameErrorSection=document.getElementById('nameErrorSection');");
+pw.println("nameErrorSection.innerHTML='';");
+pw.println("if(name.length==0)");
+pw.println("{");
+pw.println("nameErrorSection.innerHTML='Name Required';");
+pw.println("valid=false;");
+pw.println("firstInvalidComponent=frm.name;");
+pw.println("}");
+pw.println("var designationCode=frm.designationCode.value;");
+pw.println("var designationCodeErrorSection=document.getElementById('designationCodeErrorSection');");
+pw.println("designationCodeErrorSection.innerHTML='';");
+pw.println("if(designationCode==-1)");
+pw.println("{");
+pw.println("designationCodeErrorSection.innerHTML='Desigantion Required';");
+pw.println("valid=false;");
+pw.println("if(firstInvalidComponent==null) firstInvalidComponent=frm.designationCode;");
+pw.println("}");
+pw.println("");
+pw.println("");
+pw.println("var dateOfBirth=frm.dateOfBirth.value;");
+pw.println("var dateOfBirthErrorSection=document.getElementById('dateOfBirthErrorSection');");
+pw.println("dateOfBirthErrorSection.innerHTML='';");
+pw.println("if(dateOfBirth.length==0)");
+pw.println("{");
+pw.println("dateOfBirthErrorSection.innerHTML='Select date of birth';");
+pw.println("valid=false;");
+pw.println("if(firstInvalidComponent==null) firstInvalidComponent=frm.dateOfBirth;");
+pw.println("}");
+pw.println("");
+pw.println("");
+pw.println("var genderErrorSection=document.getElementById('genderErrorSection');");
+pw.println("genderErrorSection.innerHTML='';");
+pw.println("if(frm.gender[0].checked==false && frm.gender[1].checked==false)");
+pw.println("{");
+pw.println("genderErrorSection.innerHTML='Select gender';");
+pw.println("valid=false;");
+pw.println("}");
+pw.println("");
+pw.println("");
+pw.println("var basicSalary=frm.basicSalary.value.trim();");
+pw.println("var basicSalaryErrorSection=document.getElementById('basicSalaryErrorSection');");
+pw.println("basicSalaryErrorSection.innerHTML='';");
+pw.println("if(basicSalary.length==0)");
+pw.println("{");
+pw.println("basicSalaryErrorSection.innerHTML='Basic Salary required';");
+pw.println("valid=false;");
+pw.println("if(firstInvalidComponent==null) firstInvalidComponent=frm.basicSalary;");
+pw.println("}");
+pw.println("else");
+pw.println("{");
+pw.println("var v='0123456789';");
+pw.println("var e=0;");
+pw.println("var isBasicSalaryValid=true;");
+pw.println("while(e<basicSalary.length)");
+pw.println("{");
+pw.println("if(v.indexOf(basicSalary.charAt(e))==-1)");
+pw.println("{");
+pw.println("valid=false;");
+pw.println("if(firstInvalidComponent==null) firstInvalidComponent=frm.basicSalary;");
+pw.println("isBasicSalaryValid=false;");
+pw.println("break;");
+pw.println("}");
+pw.println("e++;");
+pw.println("}");
+pw.println("if(isBasicSalaryValid)");
+pw.println("{");
+pw.println("var dot=basicSalary.indexOf('.');");
+pw.println("if(dot!=-1)");
+pw.println("{");
+pw.println("var numberOfFractions=basicSalary.length-(dot+1);");
+pw.println("if(numberOfFractions>2)");
+pw.println("{");
+pw.println("basicSalaryErrorSection.innerHTML='Invalid basic salary';");
+pw.println("valid=false;");
+pw.println("if(firstInvalidComponent==null) firstInvalidComponent=frm.basicSalary;");
+pw.println("}");
+pw.println("}");
+pw.println("}");
+pw.println("}");
+pw.println("var panNumber=frm.panNumber.value.trim();");
+pw.println("var panNumberErrorSection=document.getElementById('panNumberErrorSection');");
+pw.println("panNumberErrorSection.innerHTML='';");
+pw.println("if(panNumber.length==0)");
+pw.println("{");
+pw.println("panNumberErrorSection.innerHTML='PAN number required';");
+pw.println("valid=false;");
+pw.println("if(firstInvalidComponent==null) firstInvalidComponent=frm.panNumber;");
+pw.println("}");
+pw.println("var aadharCardNumber=frm.aadharCardNumber.value.trim();");
+pw.println("var aadharCardNumberErrorSection=document.getElementById('aadharCardNumberErrorSection');");
+pw.println("aadharCardNumberErrorSection.innerHTML='';");
+pw.println("if(aadharCardNumber.length==0)");
+pw.println("{");
+pw.println("aadharCardNumberErrorSection.innerHTML='Aadhar Card number required';");
+pw.println("valid=false;");
+pw.println("if(firstInvalidComponent==null) firstInvalidComponent=frm.aadharCardNumber;");
+pw.println("}");
+pw.println("if(!valid) firstInvalidComponent.focus();");
+pw.println("return valid");
+pw.println("}");
+pw.println("");
+pw.println("function cancelAddition()");
+pw.println("{");
+pw.println("document.getElementById('cancelAddition').submit();");
+pw.println("}");
+pw.println("</script>");
+pw.println("</head>");
+pw.println("<body>");
+pw.println("<!-- Main Container Start here-->");
+pw.println("<div style='width:90hw;height:auto;border:1px solid black'>");
+pw.println("<!-- Header Starts Here-->");
+pw.println("<div style='margin:5px;width:90hw;height:auto;border:1px solid black'>");
+pw.println("<img src='/styleone/images/tm.png' style='float:left;width:40px;height:40px'>");
+pw.println("<div style='margin-top:9px;margin-bottom:9px;font-size:20pt'>");
+pw.println("Thinking Machines");
+pw.println("</div>");
+pw.println("</div>");
+pw.println("<!-- Header Ends Here-->");
+pw.println("<!-- Content-Section Starts here-->");
+pw.println("<div style='width:90hw;height:70vh;margin:5px;border:1px solid white'>");
+pw.println("<!-- Left Pannel Starts Here -->");
+pw.println("<div style='height:60vh;margin:5px;float:left;padding:5px;border:1px solid black'>");
+pw.println("<a href='/styleone/designationsView'>Designations</a><br>");
+pw.println("<b>Employees</b><br><br>");
+pw.println("<a href='/styleone/index.html'> HOME </a>");
+pw.println("</div>");
+pw.println("<!-- left Pannel Ends Here-->");
+pw.println("<!-- Right Pannel Starts Here -->");
+pw.println("<div style='height:60vh;margin-left:140px;margin-right:5px;margin-bottom:px;margin-top:5px;padding:5px;border:1px solid black'>");
+pw.println("<h2>Employee (Add Module)</h2>");
+pw.println("<form method='post' action='/styleone/addEmployee' onsubmit='return validateForm(this)'>");
+pw.println("<table>");
+pw.println("<tr>");
+pw.println("<td>Name: </td>");
+pw.println("<td><input type='text' id='name' name='name' maxlength='50' size='51'>");
+pw.println("<span id='nameErrorSection' style='color:red'></span></td>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<td>Designation: </td>");
+pw.println("<td><select id='designationCode' name='designationCode'>");
+pw.println("<option value='-1'>&lt;Select Designation&gt;</option>");
+DesignationDAO designationDAO=new DesignationDAO();
+List<DesignationDTO> designations;
+designations=designationDAO.getAll();
+
+
+designations.forEach((designation)->{
+pw.println("<option value='"+designation.getCode()+"'>"+designation.getTitle()+"</option>");
+});
+
+pw.println("</select>");
+pw.println("<span id='designationCodeErrorSection' style='color:red'></span>");
+pw.println("</td>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<td>Date of Birth: </td>");
+pw.println("<td><input type='date' id='dateOfBirth' name='dateOfBirth'>");
+pw.println("<span id='dateOfBirthErrorSection' style='color:red'></span>");
+pw.println("</td>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<td>Gender</td>");
+pw.println("<td><input type='radio' id='male' name='gender' value='M'> Male &nbsp; &nbsp; &nbsp; &nbsp;");
+pw.println("<input type='radio' id='female' name='gender' value='F'>Female <span id='genderErrorSection' style='color:red'></span>");
+pw.println("</td>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<td>Basic Salary</td>");
+pw.println("<td>");
+pw.println("<input type='text' style='text-align:right' name='basicSalary' id='basicSalary' maxlength='12' size='13'>");
+pw.println("<span id='basicSalaryErrorSection' style='color:red'></span>");
+pw.println("</td>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<td>PAN Number</td>");
+pw.println("<td>");
+pw.println("<input type='text' id='panNumber' name='panNumber' maxlength='15' size='16'>");
+pw.println("<span id='panNumberErrorSection' style='color:red'></span>");
+pw.println("</td>");
+pw.println("</tr>");
+pw.println("<tr>");
+pw.println("<td>Aadhar Card Number</td>");
+pw.println("<td>");
+pw.println("<input type='text' id='aadharCardNumber' name='aadharCardNumber' maxlength='15' size='16'>");
+pw.println("<span id='aadharCardNumberErrorSection' style='color:red'></span>");
+pw.println("</td>");
+pw.println("</tr>");
+pw.println("<td colspan='2'><button type='submit'>Add</button> &nbsp; &nbsp; &nbsp;");
+pw.println("<button type='button' onclick='cancelAddition()'>Cancel</button>");
+pw.println("</td>");
+pw.println("</table>");
+pw.println("</form>");
+pw.println("<form id='cancelAddition' method='post' action='/styleone/employeesView'>");
+pw.println("</form>");
+pw.println("</div>");
+pw.println("<!-- Right Pannel Ends Here -->");
+pw.println("</div>");
+pw.println("<!-- Content-Section Ends Here -->");
+pw.println("<!-- Footer Start Here-->");
+pw.println("<div style='width:90hw;height:auto;margin:5px;text-align:center;border:1px solid black'>");
+pw.println("&copy Thinking Machines 2023");
+pw.println("</div>");
+pw.println("<!-- Footer Ends Here -->");
+pw.println("</div>");
+pw.println("<!-- Main Container ends here");
+pw.println("</body>");
+pw.println("</html>");
+
+}catch(Exception exception)
+{
+System.out.println(exception.getMessage()); // remove after testing
+}
+}
+
+public void doPost(HttpServletRequest request,HttpServletResponse response)
+{
+doGet(request,response);
+}
+}
