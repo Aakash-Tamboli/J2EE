03f37cc90ba4f465bbe42052ed6f4aa5ce866ce8
Aakash-Tamboli <athanhuntak@gmail.com>
2023-09-26 10:17:13 +0530

    style two started

---
+++ styletwo/WEB-INF/classes/com/thinking/machines/hr/dl/DesignationDAO.java
@@ -0,0 +1,211 @@
+package com.thinking.machines.hr.dl;
+import java.util.*;
+import java.sql.*;
+public class DesignationDAO
+{
+public List<DesignationDTO> getAll() throws DAOException
+{
+List<DesignationDTO> designations;
+designations=new LinkedList<>();
+try
+{
+Connection connection=DAOConnection.getConnection();
+Statement statement=connection.createStatement();
+ResultSet resultSet;
+resultSet=statement.executeQuery("select * from designation;");
+int code;
+String title;
+DesignationDTO designationDTO;
+while(resultSet.next())
+{
+code=resultSet.getInt("code");
+title=resultSet.getString("title").trim();
+designationDTO=new DesignationDTO();
+designationDTO.setCode(code);
+designationDTO.setTitle(title);
+designations.add(designationDTO);
+}
+resultSet.close();
+statement.close();
+connection.close();
+}catch(Exception e)
+{
+throw new DAOException(e.getMessage());
+}
+return designations;
+}
+public void add(DesignationDTO designationDTO) throws DAOException
+{
+String title=designationDTO.getTitle();
+try
+{
+Connection connection=DAOConnection.getConnection();
+PreparedStatement preparedStatement;
+preparedStatement=connection.prepareStatement("select * from designation where title=?;");
+preparedStatement.setString(1,title);
+ResultSet resultSet=preparedStatement.executeQuery();
+if(resultSet.next())
+{
+resultSet.close();
+preparedStatement.close();
+connection.close();
+throw new DAOException("Designation Exists: "+title);
+}
+resultSet.close();
+preparedStatement.close();
+preparedStatement=connection.prepareStatement("insert into designation (title) values(?);",Statement.RETURN_GENERATED_KEYS);
+preparedStatement.setString(1,title);
+preparedStatement.executeUpdate();
+resultSet=preparedStatement.getGeneratedKeys();
+resultSet.next();
+int code=resultSet.getInt(1);
+resultSet.close();
+preparedStatement.close();
+connection.close();
+designationDTO.setCode(code);
+}catch(SQLException sqlException)
+{
+throw new DAOException(sqlException.getMessage());
+}
+}
+public DesignationDTO getByCode(int code) throws DAOException
+{
+try
+{
+Connection connection=DAOConnection.getConnection();
+PreparedStatement preparedStatement;
+preparedStatement=connection.prepareStatement("select * from designation where code=?;");
+preparedStatement.setInt(1,code);
+ResultSet resultSet=preparedStatement.executeQuery();
+if(resultSet.next()==false)
+{
+resultSet.close();
+preparedStatement.close();
+connection.close();
+throw new DAOException("Invalid Designation Code: "+code);
+}
+DesignationDTO designationDTO=new DesignationDTO();
+designationDTO.setCode(resultSet.getInt("code"));
+designationDTO.setTitle(resultSet.getString("title").trim());
+resultSet.close();
+preparedStatement.close();
+connection.close();
+return designationDTO;
+}catch(SQLException sqlException)
+{
+throw new DAOException(sqlException.getMessage());
+}
+}
+public void update(DesignationDTO designationDTO) throws DAOException
+{
+int code=designationDTO.getCode();
+String title=designationDTO.getTitle();
+try
+{
+Connection connection=DAOConnection.getConnection();
+PreparedStatement preparedStatement;
+preparedStatement=connection.prepareStatement("select * from designation where code=?;");
+preparedStatement.setInt(1,code);
+ResultSet resultSet=preparedStatement.executeQuery();
+if(resultSet.next()==false)
+{
+resultSet.close();
+preparedStatement.close();
+connection.close();
+throw new DAOException("Invalid Designation Code: "+code);
+}
+resultSet.close();
+preparedStatement.close();
+preparedStatement=connection.prepareStatement("select * from designation where title=? and code!=?;");
+preparedStatement.setString(1,title);
+preparedStatement.setInt(2,code);
+
+resultSet=preparedStatement.executeQuery();
+if(resultSet.next())
+{
+resultSet.close();
+preparedStatement.close();
+connection.close();
+throw new DAOException(title+" exists");
+}
+resultSet.close();
+preparedStatement.close();
+preparedStatement=connection.prepareStatement("update designation set title=? where code=?;");
+preparedStatement.setString(1,title);
+preparedStatement.setInt(2,code);
+preparedStatement.executeUpdate();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+throw new DAOException(sqlException.getMessage());
+}
+}
+public void delete(int code) throws DAOException
+{
+try
+{
+Connection connection=DAOConnection.getConnection();
+PreparedStatement preparedStatement;
+preparedStatement=connection.prepareStatement("select * from designation where code=?;");
+preparedStatement.setInt(1,code);
+ResultSet resultSet=preparedStatement.executeQuery();
+if(resultSet.next()==false)
+{
+resultSet.close();
+preparedStatement.close();
+connection.close();
+throw new DAOException("Invalid Designation");
+}
+resultSet.close();
+preparedStatement.close();
+preparedStatement=connection.prepareStatement("select id from employee where designation_code=?;");
+preparedStatement.setInt(1,code);
+resultSet=preparedStatement.executeQuery();
+if(resultSet.next())
+{
+resultSet.close();
+preparedStatement.close();
+connection.close();
+throw new DAOException("Designation Alloted for employee hence it cannot be deleted");
+}
+resultSet.close();
+preparedStatement.close();
+preparedStatement=connection.prepareStatement("delete from designation where code=?;");
+preparedStatement.setInt(1,code);
+preparedStatement.executeUpdate();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+throw new DAOException(sqlException.getMessage());
+}
+}
+public boolean designationCodeExists(int code) throws DAOException
+{
+boolean answer=false;
+try
+{
+Connection connection=DAOConnection.getConnection();
+PreparedStatement preparedStatement;
+preparedStatement=connection.prepareStatement("select title from designation where code=?;");
+preparedStatement.setInt(1,code);
+ResultSet resultSet=preparedStatement.executeQuery();
+if(resultSet.next())
+{
+answer=true;
+}
+else
+{
+answer=false;
+}
+resultSet.close();
+preparedStatement.close();
+connection.close();
+}catch(SQLException sqlException)
+{
+throw new DAOException(sqlException.getMessage());
+}
+return answer;
+}
+}
---
+++ styletwo/WEB-INF/classes/com/thinking/machines/hr/servlets/AddDesignation.java
@@ -0,0 +1,63 @@
+package com.thinking.machines.hr.servlets;
+import com.thinking.machines.hr.dl.*;
+import com.thinking.machines.hr.beans.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class AddDesignation extends HttpServlet
+{
+public void doPost(HttpServletRequest request,HttpServletResponse response)
+{
+try
+{
+DesignationBean designationBean;
+designationBean=(DesignationBean)request.getAttribute("designationBean");
+String title=designationBean.getTitle();
+DesignationDTO designation;
+designation=new DesignationDTO();
+designation.setTitle(title);
+DesignationDAO designationDAO;
+designationDAO=new DesignationDAO();
+try
+{
+designationDAO.add(designation);
+designationBean.setCode(designation.getCode());
+MessageBean messageBean;
+messageBean=new MessageBean();
+messageBean.setHeading("Designation (Add Module)"); // heading data will be given by project lead we are just programer.
+messageBean.setMessage("Designation added, add more ?");
+messageBean.setGenerateButtons(true);
+messageBean.setGenerateTwoButtons(true);
+messageBean.setButtonOneText("Yes");
+messageBean.setButtonTwoText("No");
+messageBean.setButtonOneAction("DesignationAddForm.jsp");
+messageBean.setButtonTwoAction("Designation.jsp");
+request.setAttribute("messgaeBean",messageBean);
+RequestDispatcher requestDispatcher;
+requestDispatcher=request.getRequestDispatcher("/Notifcation.jsp");
+requestDispatcher.forward(request,response);
+}catch(DAOException daoException)
+{
+ErrorBean errorBean;
+errorBean=new ErrorBean();
+errorBean.setError(daoException.getMessage());
+request.setAttribute("errorBean",errorBean);
+RequestDispatcher requestDispatcher;
+requestDispatcher=request.getRequestDispatcher("/DesignationAddForm.jsp");
+requestDispatcher.forward(request,response);
+}
+}catch(Exception exception)
+{
+System.out.println(exception.getMessage());
+RequestDispatcher requestDispatcher;
+requestDispatcher=request.getRequestDispatcher("Error.jsp");
+try
+{
+requestDispatcher.forward(request,response);
+}catch(Exception e)
+{
+// not yet decieded
+}
+}
+}
+}
