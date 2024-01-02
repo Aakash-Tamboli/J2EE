1c8470582247be7ac404005fb2a7ccb7884033a8
Aakash-Tamboli <athanhuntak@gmail.com>
2024-01-02 18:56:51 +0530

    TMJRock on going

---
+++ TMJRock/WEB-INF/classes/com/thinking/machines/hr/servlets/Customer.java
@@ -0,0 +1,39 @@
+package com.thinking.machines.hr.servlets;
+
+public class Customer implements java.io.Serializable
+{
+private String firstName;
+private String lastName;
+private int age;
+
+public void setFirstName(String firstName)
+{
+this.firstName=firstName;
+}
+
+public String getFirstName()
+{
+return this.firstName;
+}
+
+public void setLastName(String lastName)
+{
+this.lastName=lastName;
+}
+
+public String getLastName()
+{
+return this.lastName;
+}
+
+public void setAge(int age)
+{
+this.age=age;
+}
+
+public int getAge()
+{
+return this.age;
+}
+
+}
--- TMJRock/WEB-INF/classes/com/thinking/machines/hr/servlets/ServletThree.java
+++ TMJRock/WEB-INF/classes/com/thinking/machines/hr/servlets/ServletThree.java
@@ -4,6 +4,8 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.util.*;
 import com.thinking.machines.hr.dl.*;
+import com.google.gson.*;
+
 
 public class ServletThree extends HttpServlet
 {
@@ -15,12 +17,38 @@ public void doPost(HttpServletRequest request,HttpServletResponse response)
 {
 try
 {
+/*
+BufferedReader bufferedReader=request.getReader();
+StringBuffer stringBuffer=new StringBuffer();
+String str;
+while(true)
+{
+str=bufferedReader.readLine();
+if(str==null) break;
+stringBuffer.append(str);
+}
+String rawData=stringBuffer.toString();
+Gson gson=new Gson();
+Customer customer=gson.fromJson(rawData,Customer.class);
+PrintWriter pw=response.getWriter();
+response.setContentType("application/json");
+System.out.println(customer.getFirstName()+","+customer.getLastName()+","+customer.getAge());
+pw.print(gson.toJson(customer));
+*/
+
+// When Request contains non-json
 String firstName=request.getParameter("firstName");
 String lastName=request.getParameter("lastName");
 int age=Integer.parseInt(request.getParameter("age"));
-PrintWriter out=response.getWriter();
-response.setContentType("text/plain");
-out.print(firstName+","+lastName+","+age);
+System.out.println(firstName+","+lastName+","+age);
+PrintWriter pw=response.getWriter();
+response.setContentType("application/json");
+Gson gson=new Gson();
+Customer customer=new Customer();
+customer.setFirstName(firstName);
+customer.setLastName(lastName);
+customer.setAge(age);
+pw.print(gson.toJson(customer));
 }catch(Exception exception)
 {
 System.out.println("Problem At Server Side: "+exception.getMessage());
