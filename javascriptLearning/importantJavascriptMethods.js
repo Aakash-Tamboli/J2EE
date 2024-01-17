2eeb5cb74766a39f17be5c78d95b219e17fff4ff
Aakash-Tamboli <athanhuntak@gmail.com>
2024-01-17 10:13:49 +0530

    Js Learning

--- javascriptLearning/importantJavascriptMethods.js
+++ javascriptLearning/importantJavascriptMethods.js
@@ -1,3 +1,5 @@
+
+1)---------------------------Start-------------------------
 Object.keys(pass_your_json_object) it will returns an array containg all properties names.
 Object.value(pass_your_json_object) it will returns you know. all values against properties.
 
@@ -10,3 +12,13 @@ for(let key in queryStringData)
 {
 url=url+key+"="+queryStringData[key];
 }
+1)----------------------------End-----------------------
+2)----------------------------Start---------------------
+
+(html tag)domObject.hasAttribute('attributeName') -
+	it returns true if attributeName is applied on domObj(html tag)
+	otherwise it return false
+(html tag)domObject.getAttribute('attributeName') -
+	it returns value of corrsesponding attributeName which is applied on domObj(html tag)
+
+2)----------------------------End-----------------------
